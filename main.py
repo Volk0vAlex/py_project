ca2b54d0b5fe0002290e15ac59082b70c740d006
Volk0vAlex <volkov.alex1986@gmail.com>
2023-04-03 22:15:30 +0700

    test 8 function call 2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from funcs import sum_of_two_numbers
+from funcs import subtraction_of_two_numbers
 
 a = 2
 b = 22
 
 print(sum_of_two_numbers(a, b))
+
+c = 20
+d = 5
+
+print(subtraction_of_two_numbers(c,d))
