c8f80a4128ecf3cad7ca0ec7ac2acabde476bd1a
Volk0vAlex <volkov.alex1986@gmail.com>
2023-04-03 22:06:54 +0700

    test 6 add substract

--- funcs.py
+++ funcs.py
@@ -1,4 +1,8 @@
-def sum_of_two_numbers(a,b):
+def sum_of_two_numbers(a, b):
     sum_numbers = a + b
     return sum_numbers
 
+
+def subtraction_of_two_numbers(c, d):
+    substract = c - d
+    return substract
